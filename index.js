12b99ff9e3db0aff0cdd51e1af7ddff3bc3340f7
Jefferson <jeffersonpierov@gmail.com>
2023-02-09 12:09:11 -0500

    PruebaProyecto

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+//configuracion de los templates
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+app.set('view engine','ejs');
+app.set('views','./view');
+
+app.get('/home',(req,res)=>{
+    res.render('partials/dashboard')
+})
+
+app.get('/',(req,res)=>{
+    res.render('auth/login');
+})
+app.listen(PORT,()=>{
+    console.log('Hello wordl')
+})
